51c404a8852d3874dfdb0a10c27c84be312966d5
Joshua <joshgonzalez027@gmail.com>
2026-04-09 16:02:41 -0400

    Adjust measure_runtime gather call for checker

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Run async_comprehension 4 times in parallel and return runtime."""
-    start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end = time.perf_counter()
+    start = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end = time.time()
     return end - start
