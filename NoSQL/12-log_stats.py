672976b6f3af85609a4277b4d6919123fc8165df
JGonzalez-7 <joshgonzalez027@gmail.com>
2026-04-26 21:07:15 -0400

    updated log stats script to use count method for checker compatibility

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,18 +8,19 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
+    total_logs = collection.count()
     print("{} logs".format(total_logs))
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     for method in methods:
-        count = collection.count_documents({"method": method})
+        count = collection.find({"method": method}).count()
         print("\tmethod {}: {}".format(method, count))
 
-    status_check = collection.count_documents({
+    status_check = collection.find({
         "method": "GET",
         "path": "/status"
-    })
+    }).count()
     print("{} status check".format(status_check))
+    
