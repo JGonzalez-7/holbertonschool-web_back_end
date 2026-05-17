dd6e77ee9d7a324a614d3f8d54f3f860cdd6faca
JGonzalez-7 <joshgonzalez027@gmail.com>
2026-05-17 00:50:03 -0400

    fixed organized server database error handling

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -4,28 +4,25 @@ export default function readDatabase(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (error, data) => {
       if (error) {
-        reject(error);
+        reject(new Error('Cannot load the database'));
         return;
       }
 
       const lines = data
         .split('\n')
-        .map((line) => line.trim())
-        .filter((line) => line.length > 0);
+        .filter((line) => line.trim() !== '');
 
       const students = lines.slice(1);
       const fields = {};
 
       students.forEach((student) => {
-        const studentData = student.split(',');
-        const firstName = studentData[0];
-        const field = studentData[3];
+        const [firstname, , , field] = student.split(',');
 
         if (!fields[field]) {
           fields[field] = [];
         }
 
-        fields[field].push(firstName);
+        fields[field].push(firstname);
       });
 
       resolve(fields);
